7f1ce2480f7da60fdbaa2cf5301e1fefa5f8fbb6
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-09-26 23:51:29 +0530

    Add files via upload

---
+++ pointer_reassignment_strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    char *str = "Hello";   
+
+    printf("Original: %s\n", str);
+    
+    str = "World";         
+    printf("%s\n", str);
+
+    return 0;
+}
